47078bff143ba2555aa3ee36a8564286afc90839
shehroz9712 <shehrysoftologics@gmail.com>
2025-06-30 16:14:28 +0500

    Refactor CrudGeneratorServiceProvider to correct configuration file paths and improve command registration

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -3,27 +3,28 @@
 namespace Shehroz\CrudGenerator;
 
 use Illuminate\Support\ServiceProvider;
-use Shehroz\CrudGenerator\Commands\MakeCrud;
+use Shehroz\CrudGenerator\Console\MakeCrud;
 
 class CrudGeneratorServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'crud-generator');
+        // Merge the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/crud-generator.php', 'crud-generator');
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
+            // Register the make:crud command
             $this->commands([
                 MakeCrud::class,
             ]);
 
+            // Publish stub files to the user's project
             $this->publishes([
-                __DIR__ . '/Stubs' => resource_path('views/commands/crud'),
+                __DIR__ . '/../stubs' => base_path('stubs/crud-generator'),
             ], 'crud-generator-stubs');
         }
     }
 }
-
-
